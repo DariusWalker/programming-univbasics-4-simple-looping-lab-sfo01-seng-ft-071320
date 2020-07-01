8ec170db53b8e590c27b5a9cc3e792d780f74f03
Darius Walker <dw862716@ohio.edu>
2020-07-01 23:09:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,10 +25,13 @@ end
 
 def return_string_array(array)
   count = 0 
+  new array =[]
+  
   while count < array.length do 
-    array[count].to_s 
+    new.array.push(array[count].to_s)
+    
     count += 1 
   end 
-  array 
+  new_array 
   
 end
